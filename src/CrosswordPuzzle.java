09f94752402b0705bbfe4f40d083eed4e90a5fbe
IsisJ <isisofalexandria@gmail.com>
2020-03-27 22:40:49 -0700

    needs reviewing

--- src/CrosswordPuzzle.java
+++ src/CrosswordPuzzle.java
@@ -1,5 +1,33 @@
-
-public class CrosswordPuzzle
-{
+public class CrosswordPuzzle{
+	private Square[][] puzzle;
+	
+	
+	public CrosswordPuzzle(boolean[][] blackSquares) {
+		puzzle = new Square[blackSquares.length][blackSquares.length];
+		int posNumberingCount = 2;
+		
+		for(int rows=0; rows<puzzle.length;rows++) {
+			for(int cols=0; cols<puzzle.length;cols++) {
+				if(blackSquares[rows][cols]) {
+					puzzle[rows][cols] = new Square(true,0);
+				}
+				else if(toBeLabeled(rows, cols, blackSquares)){
+					puzzle[rows][cols] = new Square(false, posNumberingCount);
+					posNumberingCount = posNumberingCount+2;
+				}
+					
+				}
+			}
+		}
+	}
+	private boolean toBeLabeled(int r, int c, boolean[][] blackSquares) {
+		if(blackSquares[r][c] == blackSquares[0][c] || blackSquares[r][c] == blackSquares[r][0]) {
+			return false;
+		}
+		else if(blackSquares[r][c-1] && blackSquares[r+1][c]) {
+			return true;
+		}
+		return false;
+	}
 	
 }
--- src/Square.java
+++ src/Square.java
@@ -1,3 +1,7 @@
 public class Square{
 
+	public Square(boolean isBlack, int num) {
+		// TODO Auto-generated constructor stub
+	}
+	
 }
